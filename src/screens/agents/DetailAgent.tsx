f7e53deb7e4b82b3bd7f9e1b65240c7d7ebe6240
Julian <info@racaps.com>
2020-12-29 16:29:18 -0300

    Terminal resizable

--- src/screens/agents/DetailAgent.tsx
+++ src/screens/agents/DetailAgent.tsx
@@ -10,7 +10,12 @@ import FileManager from './FileManager'
 import ProcessManager from './ProcessManager'
 import PostExploitation from './PostExploitation'
 import { useLocation } from 'react-router-dom'
-import { FaExternalLinkAlt } from 'react-icons/fa'
+import {
+  FaExternalLinkAlt,
+  FaWindowMinimize,
+  FaWindowMaximize,
+  FaWindowRestore,
+} from 'react-icons/fa'
 import Storage from '../../util/Storage'
 
 interface AgentProps {
@@ -28,14 +33,18 @@ interface Props {
   detached?: boolean
 }
 
+type TerminalSize = 'minimizezd' | 'normal' | 'maximized'
+
 export default ({ detached }: Props) => {
   useNavigatorConfig(
     detached ? { onlyContent: true } : { title: 'Agents', noPadding: true, goBack: true },
   )
   const { height } = useWindowSize()
-  const classes = useClasses({ height })
   const [value, setValue] = useState(0)
   const { state } = useLocation<AgentProps>()
+
+  const [terminalSize, setTerminalSize] = useState<TerminalSize>('normal')
+  const classes = useClasses({ height, terminalSize, detached })
 
   const handleChange = useCallback((newValue: number) => {
     setValue(newValue)
@@ -53,46 +62,50 @@ export default ({ detached }: Props) => {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" color="default" className={classes.appbar}>
-        <Tabs
-          className={classes.tabbar}
-          value={value}
-          onChange={(_, newValue) => handleChange(newValue)}
-          indicatorColor="primary"
-          textColor="primary">
-          <Tab label="Manage" />
-          <Tab label="File Manager" />
-          <Tab label="Process Manager" />
-          <Tab label="Post-Exploitation" />
-        </Tabs>
-        {!detached && (
-          <IconButton
-            size="small"
-            onClick={() => {
-              const size = Storage.getItem<{ width: number; height: number }>('DetachedSize')
-              const sizeString = size?.height
-                ? `height=${size.height},width=${size.width}`
-                : 'height=600,width=800'
-              window.open(window.location.origin + '/detached_agent', '_blank', sizeString)
-            }}>
-            <FaExternalLinkAlt />
-          </IconButton>
-        )}
-      </AppBar>
-      <SwipeableViews className={classes.tabs} index={value} onChangeIndex={handleChange}>
-        <TabPanel value={value} index={0}>
-          <Manage />
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          <FileManager />
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-          <ProcessManager />
-        </TabPanel>
-        <TabPanel value={value} index={3}>
-          <PostExploitation />
-        </TabPanel>
-      </SwipeableViews>
+      {terminalSize !== 'maximized' && (
+        <React.Fragment>
+          <AppBar position="static" color="default" variant="outlined" className={classes.appbar}>
+            <Tabs
+              className={classes.tabbar}
+              value={value}
+              onChange={(_, newValue) => handleChange(newValue)}
+              indicatorColor="primary"
+              textColor="primary">
+              <Tab label="Manage" />
+              <Tab label="File Manager" />
+              <Tab label="Process Manager" />
+              <Tab label="Post-Exploitation" />
+            </Tabs>
+            {!detached && (
+              <IconButton
+                size="small"
+                onClick={() => {
+                  const size = Storage.getItem<{ width: number; height: number }>('DetachedSize')
+                  const sizeString = size?.height
+                    ? `height=${size.height},width=${size.width}`
+                    : 'height=600,width=800'
+                  window.open(window.location.origin + '/detached_agent', '_blank', sizeString)
+                }}>
+                <FaExternalLinkAlt />
+              </IconButton>
+            )}
+          </AppBar>
+          <SwipeableViews className={classes.tabs} index={value} onChangeIndex={handleChange}>
+            <TabPanel value={value} index={0}>
+              <Manage />
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+              <FileManager />
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+              <ProcessManager />
+            </TabPanel>
+            <TabPanel value={value} index={3}>
+              <PostExploitation />
+            </TabPanel>
+          </SwipeableViews>
+        </React.Fragment>
+      )}
       <div className={classes.terminal}>
         <Terminal
           style={{ fontWeight: 'bold', fontSize: '1em' }}
@@ -105,6 +118,26 @@ export default ({ detached }: Props) => {
           hideTopBar
           msg={state?.hostname}
         />
+        <div className={classes.terminalBtns}>
+          <IconButton
+            disabled={terminalSize === 'minimizezd'}
+            size="small"
+            onClick={() => setTerminalSize('minimizezd')}>
+            <FaWindowMinimize />
+          </IconButton>
+          <IconButton
+            disabled={terminalSize === 'normal'}
+            size="small"
+            onClick={() => setTerminalSize('normal')}>
+            <FaWindowRestore />
+          </IconButton>
+          <IconButton
+            disabled={terminalSize === 'maximized'}
+            size="small"
+            onClick={() => setTerminalSize('maximized')}>
+            <FaWindowMaximize />
+          </IconButton>
+        </div>
       </div>
     </div>
   )
@@ -119,14 +152,29 @@ const useClasses = makeStyles((theme) => ({
   tabbar: {
     flex: 1,
   },
-  root: {
-    // backgroundColor: theme.palette.background.paper,
-  },
-  tabs: ({ height }: any) => ({
+  root: ({ height, detached }: any) => ({
+    height: height - (detached ? 0 : 64),
+    display: 'flex',
+    flexDirection: 'column',
+  }),
+  tabs: {
     padding: theme.spacing(2),
-    height: height - 300,
+    flex: 1,
+  },
+  terminal: ({ terminalSize }: any) => ({
+    transition: theme.transitions.create('height', {
+      duration: theme.transitions.duration.standard,
+      easing: theme.transitions.easing.sharp,
+    }),
+    height: terminalSize === 'normal' ? 175 : terminalSize === 'maximized' ? '100%' : 24,
+    position: 'relative',
   }),
-  terminal: ({ height }: any) => ({
-    height: height - 380,
-  }),
+  terminalBtns: {
+    backgroundColor: 'white',
+    paddingRight: theme.spacing(1),
+    paddingLeft: theme.spacing(1),
+    position: 'absolute',
+    top: 0,
+    right: 0,
+  },
 }))

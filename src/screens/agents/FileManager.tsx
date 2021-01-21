0dc50e75ba24b71a87f22a8ab6ac30f0f4748c78
Mariano Rey <mariano_grana96@hotmail.com>
2021-01-21 15:49:38 -0300

    bastante avanzado

--- src/screens/agents/FileManager.tsx
+++ src/screens/agents/FileManager.tsx
@@ -1,25 +1,29 @@
-import React, { memo, useCallback, useRef } from 'react'
+import React, { memo, useCallback, useEffect, useRef, useState } from 'react'
 import { ChonkyActions, FileActionData, FullFileBrowser } from 'chonky'
 import { Card, CardActions, CircularProgress, makeStyles, Typography } from '@material-ui/core'
 import { SnackbarKey, useSnackbar } from 'notistack'
 import './chonky.css'
+import { useSocket } from '../../util/SocketContext'
+import { useNavigator } from 'material-navigator'
+import { callWs, useUser } from 'material-crud'
+import Urls from '../../util/Urls'
 
 export default memo(() => {
   const { enqueueSnackbar, closeSnackbar } = useSnackbar()
   const classes = useClasses()
+  const { setLoading } = useNavigator()
+  const { send, onError, onMessage, id } = useSocket()
+  const { headers } = useUser()
 
   const snacksRef = useRef<SnackbarKey[]>([])
   const uploadFileRef = useRef<HTMLInputElement | null>(null)
-  const files = [
-    { id: 'lht', name: 'Projects', isDir: true },
-    {
-      id: 'mcd',
-      name: 'chonky-sphere-v2.png',
-      thumbnailUrl: 'https://chonky.io/chonky-sphere-v2.png',
-    },
-  ]
+  const downloadRef = useRef<string>('')
 
-  const folderChain = [{ id: 'home', name: '/home', isDir: true }]
+  const [files, setFiles] = useState<any[]>([null])
+  const folderChain = [
+    { id: 'home', name: 'Home', openable: false },
+    { id: 'dev2', name: 'Dev2' },
+  ]
 
   const showNotification = useCallback(
     (message: string) => {
@@ -47,21 +51,98 @@ export default memo(() => {
     [classes, closeSnackbar, enqueueSnackbar],
   )
 
-  const handleActions = useCallback((action: FileActionData<any>) => {
-    const { id } = action
-    switch (id) {
-      case ChonkyActions.DownloadFiles.id:
-        console.log(action)
-        break
-      case ChonkyActions.UploadFiles.id:
-        console.log(action)
-        uploadFileRef.current?.click()
-        break
-      default:
-        console.log(id, ChonkyActions.UploadFiles.id)
-        break
-    }
-  }, [])
+  const handleSocket = useCallback(
+    (typeAction: 'list' | 'upload' | 'download', data?: any) => {
+      onMessage(async (e) => {
+        const { type, reference, content } = JSON.parse(e.data || '{}')
+        if (type === 'task.created') {
+          switch (typeAction) {
+            case 'list':
+              send({ type: 'file_manager.list_directory', directory: '/home/dev2/', reference })
+              break
+            case 'download':
+              downloadRef.current = data[0].id
+              send({
+                type: 'file_manager.download',
+                file_path: `/home/dev2/${data[0].id}`,
+                reference,
+              })
+              break
+            case 'upload':
+              // send({ type: 'file_manager.upload', target_directory: '/home/dev2/', reference })
+              console.log('subir archivo')
+              break
+          }
+        } else if (content) {
+          setLoading(false)
+          if (content.error) {
+            showNotification(content.error)
+            return
+          }
+          switch (type) {
+            case 'file_manager.list_directory.result':
+              const { files, directories } = content
+              setFiles([
+                ...files.map(({ name }: any) => ({ id: name, name })),
+                ...directories.map(({ name }: any) => ({ id: name, name, isDir: true })),
+              ])
+              break
+            case 'file_manager.download.result':
+              setLoading(true)
+              const { response } = await callWs<Blob>({
+                url: `${Urls.agents_download(id)}`,
+                responseType: 'blob',
+                params: { 'task-reference': reference },
+                headers,
+              })
+              if (response) {
+                const url = window.URL.createObjectURL(new Blob([response]))
+                const link = document.createElement('a')
+                link.href = url
+                link.setAttribute('download', downloadRef.current)
+                document.body.appendChild(link)
+                link.click()
+                link.remove()
+              }
+              setLoading(false)
+              break
+            case 'file_manager.upload.result':
+              showNotification(content)
+              break
+          }
+        }
+      })
+      onError((e) => {
+        setLoading(false)
+        showNotification('Error ocurred')
+      })
+      send({ type: 'create.task' })
+    },
+    [onMessage, onError, showNotification, send, setLoading, id, headers],
+  )
+
+  const handleActions = useCallback(
+    (action: FileActionData<any>) => {
+      const { id } = action
+      switch (id) {
+        case ChonkyActions.DownloadFiles.id:
+          handleSocket('download', action.state.selectedFilesForAction)
+          break
+        case ChonkyActions.UploadFiles.id:
+          uploadFileRef.current?.click()
+          console.log(action)
+          break
+        default:
+          console.log(id, ChonkyActions.UploadFiles.id)
+          break
+      }
+    },
+    [handleSocket],
+  )
+
+  useEffect(() => {
+    handleSocket('list')
+  }, [handleSocket])
 
   return (
     <div onContextMenu={() => false}>
